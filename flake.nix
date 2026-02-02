b4acd547d5106382e1341433bcdbd4eb4a78d31c
Jackson Terrill <jacksonterrill3@gmail.com>
2026-02-02 03:22:11 +0000

    refactor: make files semi-dendritic

--- flake.nix
+++ flake.nix
@@ -2,23 +2,58 @@
   description = "jtrrll's multipurpose web server";
 
   inputs = {
-    devenv.url = "github:cachix/devenv";
-    dotfiles.url = "github:jtrrll/dotfiles";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    ### Development dependencies ###
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    devenv = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:cachix/devenv";
+    };
+    flake-parts = {
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+      url = "github:hercules-ci/flake-parts";
+    };
+    import-tree.url = "github:vic/import-tree";
+    justix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:jtrrll/justix";
+    };
+    snekcheck = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:jtrrll/snekcheck";
+    };
+    treefmt-nix = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:numtide/treefmt-nix";
+    };
+
+    ### Service dependencies ###
     portfolio.url = "github:jtrrll/portfolio";
-    snekcheck.url = "github:jtrrll/snekcheck";
-    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   outputs =
     {
       flake-parts,
+      import-tree,
       nixpkgs,
       ...
     }@inputs:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [ ./nix ];
-      systems = nixpkgs.lib.systems.flakeExposed;
-    };
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      let
+        modules-tree = nixpkgs.lib.pipe import-tree [
+          (it: it.addPath ./modules)
+          # Matches top-level `*.nix` files and `default.nix` files that are one level deep.
+          (it: it.match "^/[^/]+\.nix$|^/[^/]+/default\.nix$")
+        ];
+      in
+      {
+        imports = [
+          ./dev_shells
+          modules-tree.result
+        ];
+
+        flake.lib.modules-tree = modules-tree;
+
+        systems = nixpkgs.lib.systems.flakeExposed;
+      }
+    );
 }
---
+++ modules/formatting.nix
@@ -0,0 +1,27 @@
+{ inputs, self, ... }:
+{
+  imports = [ inputs.treefmt-nix.flakeModule ];
+
+  perSystem =
+    { inputs', pkgs, ... }:
+    {
+      checks.snekcheck =
+        pkgs.runCommandLocal "snekcheck"
+          {
+            buildInputs = [ inputs'.snekcheck.packages.default ];
+          }
+          ''
+            find ${self}/** -exec snekcheck {} +
+            touch $out
+          '';
+      treefmt = {
+        programs = {
+          deadnix.enable = true;
+          keep-sorted.enable = true;
+          nixfmt.enable = true;
+          statix.enable = true;
+        };
+        settings.excludes = [ "*/hardware_configuration.nix" ];
+      };
+    };
+}
